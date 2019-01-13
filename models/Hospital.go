4abec5946a4db231f57b6fac11552636a9d23fde
Jirawat <paoji@icloud.com>
2019-01-13 14:29:12 +0700

    Update Hospital.go

--- models/Hospital.go
+++ models/Hospital.go
@@ -10,5 +10,6 @@ type User struct {
 	Id        bson.ObjectId `json:"id" bson:"_id"`
 	Name      string        `json:"name" bson:"name"`
 	Away      string        `json:"away" bson:"away"`
+	Link      string        `json:"link" bson:"link"`
 	CreatedAt time.Time     `bson:"createdAt"`
 }
